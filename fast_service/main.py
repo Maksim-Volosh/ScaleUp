5661f8fb72e2893a2552493a8aabfb0101a33604
Maksim Volosh <maksgoza9@gmail.com>
2025-07-11 16:10:32 +0300

    feat: parse node service

--- fast_service/main.py
+++ fast_service/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/get_node_response")
 def read_root():
-    return {"Hello": "World"}
+    response = requests.get("http://node_service:3000")
+    print(response, flush=True)
+    if response.status_code != 200:
+        print(response, flush=True)
+        return {"error": "Failed to connect to Node service"}
+    else:
+        res = response.text
+    return {"Hello": "World", "Node Service Response": res}
 
 
